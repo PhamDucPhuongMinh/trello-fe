f947ab0d96d56a837c99605b555e65c10df2a295
phamducphuongminh <phamducphuongminh0103@gmail.com>
2024-10-08 16:10:39 +0700

    Config MUI theme.

---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles'
+
+// Defaut theme: https://mui.com/material-ui/customization/default-theme/
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#34a854'
+    }
+  }
+})
+
+export default theme
