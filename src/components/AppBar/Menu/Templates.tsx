3226878437e25f03b86658c67768d6251d390ee4
phamducphuongminh <phamducphuongminh0103@gmail.com>
2024-10-17 10:49:47 +0700

    App bar.

---
+++ src/components/AppBar/Menu/Templates.tsx
@@ -0,0 +1,61 @@
+import React, { Fragment, useState } from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemText from '@mui/material/ListItemText'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Typography from '@mui/material/Typography'
+import Popover from '@mui/material/Popover'
+import { MENU } from '~/data'
+
+const Templates: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null)
+  const templateList = MENU.templates
+  const open = Boolean(anchorEl)
+  const id = open ? 'templates-popover' : undefined
+
+  return (
+    <Fragment>
+      <Button aria-describedby={id} endIcon={<ExpandMoreIcon />} onClick={e => setAnchorEl(e.currentTarget)}>
+        Templates
+      </Button>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        slotProps={{ paper: { sx: { width: 300, paddingY: 1 } } }}
+      >
+        <Box>
+          {templateList.map(item => (
+            <MenuItem key={item.id} sx={{ gap: 1.5 }}>
+              <ListItemIcon>
+                <Box
+                  sx={{
+                    backgroundColor: item.thumbnail,
+                    width: '40px',
+                    height: '32px',
+                    borderRadius: '4px'
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography variant="body2" noWrap={true} sx={{ marginBottom: 0, fontWeight: 'medium' }}>
+                  {item.name}
+                </Typography>
+              </ListItemText>
+            </MenuItem>
+          ))}
+        </Box>
+      </Popover>
+    </Fragment>
+  )
+}
+
+export default Templates
--- src/theme.tsx
+++ src/theme.tsx
@@ -18,11 +18,59 @@ declare module '@mui/material/styles' {
 
 const theme = createTheme({
   trello: {
-    appBarHeight: '48px',
-    boardBarHeight: '58px'
+    appBarHeight: '58px',
+    boardBarHeight: '60px'
   },
   colorSchemes: {
     dark: true
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: 'primary.main',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-:hover': {
+            backgroundColor: 'primary.light'
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          fontSize: '0.875rem',
+          color: theme.palette.primary.main,
+          '.MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.light
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.main
+          }
+        })
+      }
+    }
   }
 })
 
