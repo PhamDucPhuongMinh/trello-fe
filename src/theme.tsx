5467d5e54a0db0f5215e7616ff580778450774af
phamducphuongminh <phamducphuongminh0103@gmail.com>
2024-10-21 11:16:20 +0700

    Board content.

--- src/theme.tsx
+++ src/theme.tsx
@@ -5,6 +5,7 @@ declare module '@mui/material/styles' {
     trello: {
       appBarHeight: string
       boardBarHeight: string
+      boardContentHeight: string
     }
   }
   // allow configuration using `createTheme`
@@ -12,14 +13,20 @@ declare module '@mui/material/styles' {
     trello?: {
       appBarHeight?: string
       boardBarHeight?: string
+      boardContentHeight?: string
     }
   }
 }
 
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+
 const theme = createTheme({
   trello: {
     appBarHeight: '58px',
-    boardBarHeight: '60px'
+    boardBarHeight: '60px',
+    boardContentHeight: BOARD_CONTENT_HEIGHT
   },
   colorSchemes: {
     dark: true
@@ -36,7 +43,7 @@ const theme = createTheme({
             backgroundColor: '#dcdde1',
             borderRadius: '8px'
           },
-          '*::-webkit-scrollbar-:hover': {
+          '*::-webkit-scrollbar-thumb:hover': {
             backgroundColor: 'white'
           }
         }
@@ -56,6 +63,15 @@ const theme = createTheme({
           color: theme.palette.primary.main,
           fontSize: '0.875rem'
         })
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-body1': {
+            fontSize: '0.875rem'
+          }
+        }
       }
     },
     MuiOutlinedInput: {
