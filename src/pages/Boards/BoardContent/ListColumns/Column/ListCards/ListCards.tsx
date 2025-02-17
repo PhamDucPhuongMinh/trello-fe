bfdc4f8e7c2262694ac6ebfe44d3c943836ee39c
phamducphuongminh <phamducphuongminh0103@gmail.com>
2025-02-17 14:43:54 +0700

    UI create add column and card form.

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.tsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.tsx
@@ -13,7 +13,7 @@ const ListCards: React.FC<Props> = ({ cards }) => {
     <SortableContext items={cards.map(item => item._id)} strategy={verticalListSortingStrategy}>
       <Box
         sx={{
-          p: '0 5px',
+          p: '0 5px 5px 5px',
           m: '0 5px',
           display: 'flex',
           flexDirection: 'column',
