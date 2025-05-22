d2b255bdc3c401352c404bee73e9adb94ae3341d
phamducphuongminh <phamducphuongminh0103@gmail.com>
2025-05-22 12:08:34 +0700

    Logout & Refresh token with axios interceptors

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,13 @@
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { interceptorLoadingElements } from './formatter'
+import { logoutUserAPI } from '~/redux/currentUser/currentUserSlice'
+import { AppDispatch } from '~/redux/store'
+import { refreshTokenAPI } from '~/apis'
+
+let axiosDispatchReduxStore: AppDispatch | null = null
+export const injectAxiosDispatchReduxStore = (dispatchReduxStore: AppDispatch) =>
+  (axiosDispatchReduxStore = dispatchReduxStore)
 
 const axiosInstance = axios.create()
 
@@ -17,6 +24,8 @@ axiosInstance.interceptors.request.use(
   }
 )
 
+let refreshTokenPromise: Promise<string | void> | null = null
+
 axiosInstance.interceptors.response.use(
   response => {
     interceptorLoadingElements(false)
@@ -24,8 +33,41 @@ axiosInstance.interceptors.response.use(
   },
   error => {
     interceptorLoadingElements(false)
+    const { response, config: originalRequest } = error
 
-    const { response } = error
+    // TH1: Nếu có lỗi 401 thì gọi api Logout
+    if (response.status === 401) {
+      if (axiosDispatchReduxStore) {
+        axiosDispatchReduxStore(logoutUserAPI(false))
+      }
+    }
+
+    // TH2: Nếu có lỗi 410 thì gọi api Refresh Token
+    if (response.status === 410 && !originalRequest._retry) {
+      originalRequest._retry = true
+      if (!refreshTokenPromise) {
+        // Tạo promise call api refresh token
+        // Nếu có nhiều request thì chỉ gọi 1 lần
+        refreshTokenPromise = refreshTokenAPI()
+          .then(data => {
+            return data.accessToken
+          })
+          .catch(error => {
+            if (axiosDispatchReduxStore) {
+              axiosDispatchReduxStore(logoutUserAPI(false))
+              return Promise.reject(error)
+            }
+          })
+          .finally(() => {
+            refreshTokenPromise = null
+          })
+      }
+
+      return refreshTokenPromise.then(() => {
+        return axiosInstance(originalRequest)
+      })
+    }
+
     const errorMessage = response?.data?.message || error.message
     // Mã lỗi 410 để Refresh Token
     if (response.status !== 410) {
